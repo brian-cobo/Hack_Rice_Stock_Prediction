d17c982a0dd75e57be936a1d68d727b510ea2fb3
Brian Cobo <cobo.brian97@gmail.com>
2019-09-21 18:01:43 -0500

    Extracting Article infro from web

--- Webscraper.py
+++ Webscraper.py
@@ -36,33 +36,24 @@ tsla_quarterly_reports = ['https://www.sec.gov/Archives/edgar/data/1318605/00015
            'https://www.sec.gov/Archives/edgar/data/1318605/000119312510188792/d10q.htm']
 
 class WebScraper:
-    def find_title_from_article(self, soup):
-        """Finds Title tag from HTML"""
-        return soup.find_all('title')[0].get_text()
+    def extract_date_from_article(self, words):
+        return words.split('ended ')[1]
 
-    def find_author_from_article(self, soup):
-        """Finds Span tag, and name property from HTML"""
-        return soup.find_all('span', itemprop='name')[0].get_text()
+    def clean_article(self, article):
+        article = " ".join(article.split())  # Remove extra white spaces
 
-    def find_publish_date_and_time_from_article(self, soup):
-        """Finds Time tag and date published property from
-            HTML and separates the date and time"""
-        time = soup.find_all('time', itemprop='datePublished')
-        published_time = ''
-        for i in time:
-            published_time += i.get_text()
-        published_time = published_time.split()
-        date_published = published_time[0]
-        time_published = (' ').join(published_time[2:4])
-        return (date_published, time_published)
 
     def find_article_content(self, soup):
         """Finds Article by looking for P tags in HTML"""
+        date = None
         tag = soup.find_all('p')
         article = ''
         for i in tag:
+            if 'for the quarterly period ended' in i.get_text().lower():
+                date = self.extract_date_from_article(i.get_text().lower())
             article += (i.get_text())
-        return [article]
+        article = self.clean_article(article)
+        return date, [article]
 
     def scrape_article_from_web(self, article_URL):
         """Given a url, it calls out to other functions to extract article info
@@ -70,64 +61,37 @@ class WebScraper:
         try:
             page = requests.get(article_URL)
             soup = BeautifulSoup(page.content, 'html.parser')
-            title = self.find_title_from_article(soup)
-            author = self.find_author_from_article(soup)
-            date_published, time_published = self.find_publish_date_and_time_from_article(soup)
-            article_content = self.find_article_content(soup)
-
-            article_extracted_info = {'URL': str(article_URL),
-                                      'Title': title,
-                                      'Author': author,
-                                      'Date_Published': date_published,
-                                      'Time_Published': time_published,
-                                      'Article': article_content}
-            return (article_extracted_info)
+            date, article_content = self.find_article_content(soup)
+            print(date)
+            print(article_content)
+            # article_extracted_info = {'URL': str(article_URL),
+            #                           'Title': title,
+            #                           'Author': author,
+            #                           'Date_Published': date_published,
+            #                           'Time_Published': time_published,
+            #                           'Article': article_content}
+            #return (article_extracted_info)
         except Exception as e:
             print(e)
 
-    def find_articles_from_search_URL(self, search_URL, numPages=1):
+    def find_articles_from_search_URL(self, search_URLs):
         """Given a search URL, it finds all the article URLs and sends them to get extracted"""
         allArticles = {}
 
-        path = (f'{os.getcwd()}/Articles/')
-        fileName = path + 'Article_Info.csv'
+        path = (f'{os.getcwd()}/QuarterlyReports/')
+        fileName = path + 'TSLA_Quarterly_Reports.csv'
 
         if not os.path.exists(path):  # Create path if it doesnt exist
             os.makedirs(path)
 
-        for i in range(1, numPages+1):
+        for URL in search_URLs:
             try:
-                if numPages > 1:
-                    URL = search_URL + f'?page={i}'
-                else:
-                    URL = search_URL
-                page = requests.get(URL)
-                soup = BeautifulSoup(page.content, 'html.parser')
-                print(soup)
-                exit(0)
-                links = soup.findAll('a', attrs={'href': re.compile("^https://")})
-                print(links)
-                exit(0)
-                for link in links:
-                    newLink = link.get('href')
-                    if ('twitter.com' not in newLink and
-                            'facebook.com' not in newLink and
-                            'linkedin.com' not in newLink and
-                            'ibtimes.tumblr.com' not in newLink):
-                        print('Analyzing link:', newLink)
-                        articleInfo = self.scrape_article_from_web(newLink)
-                        if articleInfo:  # Ensure article info exists
-                            if articleInfo['URL'] not in allArticles:  # Check if article has been saved already
-                                allArticles[articleInfo['URL']] = {'Title': articleInfo['Title'],
-                                                                   'Author': articleInfo['Author'],
-                                                                   'Date_Published': articleInfo['Date_Published'],
-                                                                   'Time_Published': articleInfo['Time_Published'],
-                                                                   'Article': articleInfo['Article']}
-                            self.write_dict_to_csv(allArticles, fileName)
+                articleInfo = self.scrape_article_from_web(URL)
+                #  self.write_dict_to_csv(allArticles, fileName)
             except Exception as e:
                 self.write_dict_to_csv(allArticles, fileName)
-                print(f'Error loading info from page {i}:', e)
-            print('Finished Creating', fileName, 'for page', i, '\n')
+                print(f'Error loading info from page {URL}:', e)
+            print('Finished Creating', fileName, 'for page', URL, '\n')
 
     def write_dict_to_csv(self, articleInfo, fileName):
         with open(fileName, 'w') as csv_file:
@@ -144,9 +108,9 @@ class WebScraper:
 def run_webscraper_program():
     print('Starting Web Scraper')
     #search_url = "https://www.ibtimes.com/search/site/tsla"
-    search_url = 'https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK=0001318605&type=10-Q&dateb=&owner=exclude&count=100'
+    #search_url = 'https://www.sec.gov/cgi-bin/browse-edgar?action=getcompany&CIK=0001318605&type=10-Q&dateb=&owner=exclude&count=100'
     scraper = WebScraper()
-    scraper.find_articles_from_search_URL(search_url, numPages=1)
+    scraper.find_articles_from_search_URL(tsla_quarterly_reports)
     print('Finished Web Scraper')
 
 run_webscraper_program()
